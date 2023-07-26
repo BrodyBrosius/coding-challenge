02da6dc46a5716977a6bb8857a26b9d5c0b329c1
BrodyBrosius <mcochran477@gmail.com>
2023-07-26 19:04:25 -0400

    Added build-test.sh

--- elevator/ElevatorChallenge/build-test.sh
+++ elevator/ElevatorChallenge/build-test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 GREEN=$(tput setaf 2)
 NC=$'\e[0m'
-echo "${GREEN}Welcome to the Advanced Elevator Experience!${NC}"
-sleep 5
 echo "${GREEN}Building application...${NC}"
 sleep 5
 dotnet clean
@@ -13,6 +11,5 @@ cd "ElevatorChallenge.Tests"
 dotnet test
 cd ..
 echo "${GREEN}=============================================================${NC}"
-echo "${GREEN}Tests complete! Running application...${NC}"
+echo "${GREEN}Testing and building complete, exiting...${NC}"
 sleep 5
-dotnet run
